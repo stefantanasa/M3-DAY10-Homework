87bf5c5dab2eac4f48d3a4f9ed644d013f6f1e2a
stefantanasa <liketanasa@gmail.com>
2022-04-15 12:41:38 +0100

    POST/GET/PUT/DELETE METHODS

--- javascript.js
+++ javascript.js
@@ -1,30 +1,32 @@
 console.log("This is the add movie page");
 
-let titleValue = document.querySelector("#title").value;
-let descriptionValue = document.querySelector("#description").value;
-let categoryValue = document.querySelector("#category").value;
-let coverValue = document.querySelector("#cover").value;
-
 // handle submit
 
 const handleSubmit = (event) => {
     event.preventDefault();
+    let titleValue = document.querySelector("#title").value;
+    let descriptionValue = document.querySelector("#description").value;
+    let categoryValue = document.querySelector("#category").value;
+    let coverValue = document.querySelector("#cover").value;
+
+    // object to be sent
+    const movieObject = {
+        name: titleValue,
+        description: descriptionValue,
+        category: categoryValue,
+        imageUrl: coverValue,
+    };
+
+    getData(movieObject, "POST");
+    console.log(movieObject);
     console.log("ok");
 };
 
-// object to be sent
-const movieObject = {
-    name: titleValue,
-    description: descriptionValue,
-    category: categoryValue,
-    imageUrl: coverValue,
-};
-
-let getData = async() => {
+let getData = async(object, method, id = "") => {
     let response = await fetch(
-        "https://striveschool-api.herokuapp.com/api/movies", {
-            method: "POST",
-            body: JSON.stringify(movieObject),
+        `https://striveschool-api.herokuapp.com/api/movies/${id}`, {
+            method: method,
+            body: JSON.stringify(object),
             headers: {
                 Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwOWZmNTRjZmY1ZjAwMTU5MGJkYWUiLCJpYXQiOjE2NTAwMTAwMTEsImV4cCI6MTY1MTIxOTYxMX0.gUnkRagRPNMgM7QL-dmazjZn6f0EMEVrADfen5CYDOw",
                 "Content-Type": "Application/Json",
@@ -34,5 +36,3 @@ let getData = async() => {
     data = await response.json();
     console.log(data);
 };
-
-console.log(movieObject);
